641acc15b22a9dd0ab1a62fb5714776ffce00940
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2021-12-05 22:31:53 +0000

    End of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,39 +1,24 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from './services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
   title = 'The Dating App';
-  users: any;
-  subs: Array<any> = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit() {
-    this.getUsers()
+    this.setCurrentUser();
   }
 
-  getUsers() {
-    this.subs.push(this.http.get("https://localhost:5001/api/users").subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    }));
-  }
-
-  ngOnDestroy() {
-    console.log("Checking subscriptions");
-    if (this.subs && this.subs.length > 0) {
-      console.log(`Unsubscribing ${this.subs.length} subscriptions`);
-      this.subs.map(sub => {
-        try {
-          sub.unsubscribe();
-        } catch(ex) {}
-      })
-    }
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem("user"));
+    this.accountService.setCurrentUser(user);
   }
 }
